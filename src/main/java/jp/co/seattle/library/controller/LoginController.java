2d9957ca3f8c5054ba1249ee9bbc227caaf945d4
user <user@kamanoMacBook-Air.local>
2022-04-18 15:45:51 +0900

    ご確認よろしくお願いします。

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jp.co.seattle.library.dto.UserInfo;
 import jp.co.seattle.library.service.BooksService;
 import jp.co.seattle.library.service.UsersService;
 
@@ -17,40 +18,40 @@ import jp.co.seattle.library.service.UsersService;
  */
 @Controller /** APIの入り口 */
 public class LoginController {
-    final static Logger logger = LoggerFactory.getLogger(LoginController.class);
+	final static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    @Autowired
-    private BooksService booksService;
-    @Autowired
-    private UsersService usersService;
+	@Autowired
+	private BooksService booksService;
+	@Autowired
+	private UsersService usersService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String first(Model model) {
-        return "login"; //jspファイル名
-    }
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String first(Model model) {
+		return "login"; // jspファイル名
+	}
 
-    /**
-     * ログイン処理
-     *
-     * @param email メールアドレス
-     * @param password パスワード
-     * @param model
-     * @return　ホーム画面に遷移
-     */
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(
-            @RequestParam("email") String email,
-            @RequestParam("password") String password,
-            Model model) {
+	/**
+	 * ログイン処理
+	 *
+	 * @param email    メールアドレス
+	 * @param password パスワード
+	 * @param model
+	 * @return ホーム画面に遷移
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
 
-        // TODO 下記のコメントアウトを外してサービスクラスを使用してください。
-        //        UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
+		// TODO 下記のコメントアウトを外してサービスクラスを使用してください。
+		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
 
-        // TODO パスワードとメールアドレスの組み合わせ存在チェック実装
-
-        // 本の情報を取得して画面側に渡す
-        model.addAttribute("bookList", booksService.getBookList());
-        return "home";
-
-    }
+		// TODO パスワードとメールアドレスの組み合わせ存在チェック実装
+		if (selectedUserInfo == null) {
+			model.addAttribute("errorMessage", "ログインできませんでした。");
+			return "login";
+		} else {
+			// 本の情報を取得して画面側に渡す
+			model.addAttribute("bookList", booksService.getBookList());
+			return "home";
+		}
+	}
 }
