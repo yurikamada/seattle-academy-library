c2b381ec244e0a26ed3307071dec293688d615a3
user <user@kamanoMacBook-Air.local>
2022-05-13 17:54:54 +0900

    ご確認よろしくお願いします。

--- src/main/java/jp/co/seattle/library/controller/DeleteBookController.java
+++ src/main/java/jp/co/seattle/library/controller/DeleteBookController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -11,14 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jp.co.seattle.library.service.BooksService;
+
 /**
  * 削除コントローラー
  */
-@Controller //APIの入り口
+@Controller // APIの入り口
 public class DeleteBookController {
-    final static Logger logger = LoggerFactory.getLogger(DeleteBookController.class);
+	final static Logger logger = LoggerFactory.getLogger(DeleteBookController.class);
 
-    /**
+	@Autowired
+	private BooksService booksService;
+
+	/**
      * 対象書籍を削除する
      *
      * @param locale ロケール情報
@@ -33,8 +39,11 @@ public class DeleteBookController {
             @RequestParam("bookId") Integer bookId,
             Model model) {
         logger.info("Welcome delete! The client locale is {}.", locale);
-
-        return "";
+        
+        booksService.deleteBook(bookId);
+    	// 本の情報を取得して画面側に渡す
+        model.addAttribute("bookList", booksService.getBookList());
+        return "home";
 
     }
 
--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -72,4 +72,16 @@ public class BooksService {
 
         jdbcTemplate.update(sql);
     }
+    
+    /**
+     * 書籍を削除する
+     *
+     * @param bookId 書籍ID
+     */
+    public void deleteBook(int bookId) {
+
+        String sql = "delete from books where id = '"+ bookId +"';";
+
+        jdbcTemplate.update(sql);
+    }
 }
